1cc34f8fc4b8a3599fd8121dedeeff97bc3809e3
jaweria bibi abdullah <jaweriabibi4@gmail.com>
2025-05-06 16:18:12 +0500

    resolve serverless issue

--- index.js
+++ index.js
@@ -1,54 +1,31 @@
-// require("dotenv").config();
-// const express = require("express");
-// const app = express();
-// const connectDB = require("./db/connect");
-
-
-
-// const PORT = process.env.PORT;
-
-// const product_routes = require("./routes/products");
-
-// app.get("/", (req, res) => {
-//   res.send("Hello World");
-// });
-
-// app.use("/api/products", product_routes);
-
-// const start = async () => {
-//   try {
-//     await connectDB(process.env.MONGO_URI);
-//     app.listen(PORT, () => {
-//       console.log(`Server is running on port ${PORT}`);
-//     });
-//   } catch (error) {
-//     console.log(error);
-//   }
-// };
-// start();
-
-
-
-
 require("dotenv").config();
 const express = require("express");
-const serverless = require("serverless-http");
-const connectDB = require("../db/connect");
-const product_routes = require("../routes/products");
-
 const app = express();
-
+const connectDB = require("./db/connect");
+const serverless = require("serverless-http");
 app.use(express.json()); // For parsing JSON
-app.use("/api/products", product_routes);
+
+
+const PORT = process.env.PORT;
+
+const product_routes = require("./routes/products");
 
 app.get("/", (req, res) => {
-  res.send("Hello World from Vercel");
+  res.send("Hello World");
 });
 
-// Connect to DB once at cold start
-connectDB(process.env.MONGO_URI)
-  .then(() => console.log("Connected to MongoDB"))
-  .catch((err) => console.log("MongoDB connection error:", err));
+app.use("/api/products", product_routes);
 
-// Export as serverless function
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+start();
+
 module.exports.handler = serverless(app);
